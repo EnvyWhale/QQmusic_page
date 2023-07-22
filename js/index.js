432dd18f0e49c57f3360be778c93a76354b5af75
yangxiaosen <1553141879@qq.com>
2023-07-22 10:53:00 +0800

    优化js逻辑代码，减少代码冗余

--- js/index.js
+++ js/index.js
@@ -1,55 +1,50 @@
 $(function () {
-    $(".gdtj").mouseenter(function(){
-        $(".gdtj>div:eq(0),.gdtj>div:eq(1)").fadeIn(300);
-    });
-    $(".gdtj").mouseleave(function(){
-        $(".gdtj>div:eq(0),.gdtj>div:eq(1)").fadeOut(300);
-    });
-    // 监听鼠标移动到歌单推荐内两侧功能键的隐藏和显示
-    $(".box2").hover(function(){
+  // 歌单推荐部分
+  // 给歌单推进板块添加鼠标滑入事件，显示隐藏两侧点击按钮
+  showOrhide(".gdtj")
+  // 监听鼠标移动到歌单推荐内两侧功能键的隐藏和显示
+  showOrhide(".box2")
+  // 监听鼠标移动到精彩推荐内两侧功能键的隐藏和显示
+  showOrhide(".jctj")
+
+  function showOrhide(dom) {
+    $(dom).mouseenter(() => {
       // 移入显示
-      $(".box2>div:eq(0),.box2>div:eq(1)").fadeIn(300);
-    },function(){
+      $(`${dom}>div:eq(0),${dom}>div:eq(1)`).fadeIn(300);
+    })
+    $(dom).mouseleave(() => {
       // 移出隐藏
-      $(".box2>div:eq(0),.box2>div:eq(1)").fadeOut(300);
+      $(`${dom}>div:eq(0),${dom}>div:eq(1)`).fadeOut(300);
     });
-    // 监听鼠标移动到精彩推荐内两侧功能键的隐藏和显示
-    $(".jctj").hover(function(){
-      // 移入显示
-      $(".jctj>div:eq(0),.jctj>div:eq(1)").fadeIn(300);
-    },function(){
-      // 移出隐藏
-      $(".jctj>div:eq(0),.jctj>div:eq(1)").fadeOut(300);
-    });
-    //监听顶部下拉菜单的鼠标移动事件
-    var jtb1 = document.getElementsByClassName("openvip")[0];
-    var jtb2 = document.getElementsByClassName("opentake")[0];
-    jtb1.onmouseenter = function () {
-      var jt = document.getElementsByClassName("jt1")[0];
-      jt.style.transform = "rotate(180deg)";
-      var vipul = document.getElementsByClassName("vipul")[0];
-      vipul.style.display = "block";
-    };
-    jtb1.onmouseleave = function () {
-      var jt = document.getElementsByClassName("jt1")[0];
-      jt.style.transform = "rotate(0deg)";
-      var vipul = document.getElementsByClassName("vipul")[0];
-      vipul.style.display = "none";
-    };
-    jtb2.onmouseenter = function () {
-      var jt = document.getElementsByClassName("jt2")[0];
-      jt.style.transform = "rotate(180deg)";
-      var takeul = document.getElementsByClassName("takeul")[0];
-      takeul.style.display = "block";
-    };
-    jtb2.onmouseleave = function () {
-      var jt = document.getElementsByClassName("jt2")[0];
-      jt.style.transform = "rotate(0deg)";
-      var takeul = document.getElementsByClassName("takeul")[0];
-      takeul.style.display = "none";
-    };
-    //歌单推荐动态添加到页面
-    //1定义歌单数组，存放歌曲信息
+  }
+
+
+  //监听顶部下拉菜单的鼠标移动事件
+  var jtb1 = $(".openvip")[0];
+  var jtb2 = $(".opentake")[0];
+  var jt = $(".jt1")[0];
+  var jt = $(".jt2")[0];
+  var vipul = $(".vipul")[0];
+  var takeul = $(".takeul")[0];
+
+  jtb1.onmouseenter = function () {
+    jt.style.transform = "rotate(180deg)";
+    vipul.style.display = "block";
+  };
+  jtb1.onmouseleave = function () {
+    jt.style.transform = "rotate(0deg)";
+    vipul.style.display = "none";
+  };
+  jtb2.onmouseenter = function () {
+    jt.style.transform = "rotate(180deg)";
+    takeul.style.display = "block";
+  };
+  jtb2.onmouseleave = function () {
+    jt.style.transform = "rotate(0deg)";
+    takeul.style.display = "none";
+  };
+  //歌单推荐动态添加到页面
+  //1定义歌单数组，存放歌曲信息
   var arr = [
     [
       {
@@ -119,143 +114,129 @@ $(function () {
     ],
   ];
   //创建当前歌单的改变事件
-  function move(s) {
+  function render(s) {
     $(".gdtjul2 li").remove();
-    for (var i = 0; i < s.length; i++) {
-        $(".gdtjul2 li>img").css("url",`${s[i].imgUrl}`);
+    for (const item of arr[s]) {
+      // console.log(item);
+      $(".gdtjul2 li>img").css("url", `${item.imgUrl}`);
       $(".gdtjul2").append(
         $(`<li>
             <div class="imgbox">
-            <img src="${s[i].imgUrl}">
+            <img src="${item.imgUrl}">
             </div>
             <div class="mask"></div>
             <div class="maskicon"></div>
-            <a><span>${s[i].title}</span></a><br>
-            <span>${s[i].num}</span>
+            <a><span>${item.title}</span></a><br>
+            <span>${item.num}</span>
           </li>`)
       );
     }
+
     //监听鼠标移动到歌单上触发的播放按钮显示隐藏事件
     //鼠标移上去显示
-    $(".maskicon").mouseenter(function(){
-        var a=$(".maskicon").index(this);
-        $(".imgbox img").eq(a).css("transform","scale(1.2)");
-        $(".imgbox img").eq(a).css("transition","500ms");
-        $(this).css("opacity","1");
-        $(this).prev().css("opacity","0.3");
+    $(".maskicon").mouseenter(function () {
+      var a = $(".maskicon").index(this);
+      $(".imgbox img").eq(a).css("transform", "scale(1.2)");
+      $(".imgbox img").eq(a).css("transition", "500ms");
+      $(this).css("opacity", "1");
+      $(this).prev().css("opacity", "0.3");
     });
     //鼠标移开隐藏
-    $(".maskicon").mouseleave(function(){
-        var a=$(".maskicon").index(this);
-        $(".imgbox img").eq(a).css("transform","scale(1)");
-        $(".imgbox img").eq(a).css("transition","500ms");
-        $(this).css("opacity","0");
-        $(this).prev().css("opacity","0");
+    $(".maskicon").mouseleave(function () {
+      var a = $(".maskicon").index(this);
+      $(".imgbox img").eq(a).css("transform", "scale(1)");
+      $(".imgbox img").eq(a).css("transition", "500ms");
+      $(this).css("opacity", "0");
+      $(this).prev().css("opacity", "0");
 
     });
+    // 更改li圆点样式
+    $(`.gdtjul3 li:eq(${s})`).css("opacity", "0.7");
+    $(".gdtjul3 li").not($(`.gdtjul3 li:eq(${s})`)).css("opacity", "0.1");
   }
+
+
   //初始歌单的加载，默认为歌单1
   var s = 0;
-  move(arr[s]);
+  render(s);
+
   //监听歌单下面目录圆点的点击事件，切换到相应歌单页面
   $(".gdtjul3 li").click(function () {
-    $(".gdtjul3 li").not(this).css("opacity","0.1");
-    $(this).css("opacity","0.7");
+    $(".gdtjul3 li").not(this).css("opacity", "0.1");
+    $(this).css("opacity", "0.7");
     s = $(".gdtjul3 li").index(this);
-    move(arr[s]);
+    console.log(s);
+    render(s);
   });
   //监听歌单推荐两侧的切换按钮点击事件，切换到相应歌单页面
   //向前切换
-  $(".mid1").click(function(){
-      if(s==0){
-          s=2;
-          move(arr[s]);
-          $(".gdtjul3 li:eq(2)").css("opacity","0.7");
-          $(".gdtjul3 li").not($(".gdtjul3 li:eq(2)")).css("opacity","0.1");
-      }
-      else if(s==1){
-        s=0;
-        move(arr[s]);
-          $(".gdtjul3 li:eq(0)").css("opacity","0.7");
-          $(".gdtjul3 li").not($(".gdtjul3 li:eq(0)")).css("opacity","0.1");
-      }
-      else if(s==2){
-        s=1;
-        move(arr[s]);
-          $(".gdtjul3 li:eq(1)").css("opacity","0.7");
-          $(".gdtjul3 li").not($(".gdtjul3 li:eq(1)")).css("opacity","0.1");
-      }
-      return false;
+  $(".mid1").click(function () {
+    if (s == 0) s = 2
+    else s--
+    render(s)
   });
   //向后切换
-  $(".mid2").click(function(){
-    if(s==0){
-        s=1;
-        move(arr[s]);
-        $(".gdtjul3 li:eq(1)").css("opacity","0.7");
-        $(".gdtjul3 li").not($(".gdtjul3 li:eq(1)")).css("opacity","0.1");
+  $(".mid2").click(function () {
+    if (s == 2) s = 0
+    else s++
+    render(s)
+  });
+
+  // 定义哪个小圆点控制轮播
+  var i = 0;
+  //创建定时器，对精彩推荐进行定时操作，改变推荐内容
+  $(".jctjitem li").eq(0).css("opacity", "0.6");
+  var time_swiper
+  function swiper() {
+    time_swiper = setInterval(() => {
+      i++
+      if (i == 4) {
+        i = 0
+        $(".bannerlist").css("left", "10px");
+      } else {
+        $(".bannerlist").animate({ left: '-=1320px' });
+      }
+      $(".jctjitem li").eq(i).css("opacity", "0.6");
+      $(".jctjitem li").eq(i).siblings().css("opacity", "0.3");
+
+    }, 2000);
+  }
+  swiper()
+  $(".mid6").click(function () {
+    clearInterval(time_swiper)
+    if (i < 3) {
+      i++;
+      $(".bannerlist").animate({ left: '-=1320px' });
+      $(".jctjitem li").eq(i).css("opacity", "0.6");
+      $(".jctjitem li").eq(i).siblings().css("opacity", "0.3");
     }
-    else if(s==1){
-      s=2;
-      move(arr[s]);
-        $(".gdtjul3 li:eq(2)").css("opacity","0.7");
-        $(".gdtjul3 li").not($(".gdtjul3 li:eq(2)")).css("opacity","0.1");
+    else if (i = 3) {
+      i = 0;
+      $(".bannerlist").css("left", "10px");
+      $(".jctjitem li").eq(0).css("opacity", "0.6");
+      $(".jctjitem li").eq(0).siblings().css("opacity", "0.3");
     }
-    else if(s==2){
-      s=0;
-      move(arr[s]);
-        $(".gdtjul3 li:eq(0)").css("opacity","0.7");
-        $(".gdtjul3 li").not($(".gdtjul3 li:eq(0)")).css("opacity","0.1");
+    swiper()
+  });
+  $(".mid5").click(function () {
+    clearInterval(time_swiper)
+    if (i > 0) {
+      i--;
+      var p = $(".bannerlist").css("left");
+      console.log(p);
+      console.log(i);
+      $(".bannerlist").animate({ left: '+=1320px' });
+      $(".jctjitem li").eq(i).css("opacity", "0.6");
+      $(".jctjitem li").eq(i).siblings().css("opacity", "0.3");
+
     }
-    return false;
-});
-var i=0;
-    //创建定时器，对精彩推荐进行定时操作，改变推荐内容
-    $(".jctjitem li").eq(0).css("opacity","0.6");
-    setInterval(function(){
-      if(i<3){
-        i++;
-        $(".bannerlist").animate({left:'-=1320px'});
-        $(".jctjitem li").eq(i).css("opacity","0.6");
-        $(".jctjitem li").eq(i).siblings().css("opacity","0.3");
-      }else{
-        i=0;
-        $(".jctjitem li").eq(0).css("opacity","0.6");
-        $(".jctjitem li").eq(0).siblings().css("opacity","0.3");
-        $(".bannerlist").css("left","10px");
-      }
-    },4000);
-    $(".mid6").click(function(){
-      if(i<3){
-        i++;
-        $(".bannerlist").animate({left:'-=1320px'});
-        $(".jctjitem li").eq(i).css("opacity","0.6");
-        $(".jctjitem li").eq(i).siblings().css("opacity","0.3");
-      }
-      else if(i=3) {
-        i=0;
-        $(".bannerlist").css("left","10px");
-        $(".jctjitem li").eq(0).css("opacity","0.6");
-        $(".jctjitem li").eq(0).siblings().css("opacity","0.3");
-      }
-    });
-    $(".mid5").click(function(){
-      if(i>0){
-        i--;
-        var p=$(".bannerlist").css("left");
-     console.log(p);
-     console.log(i);
-        $(".bannerlist").animate({left:'+=1320px'});
-        $(".jctjitem li").eq(i).css("opacity","0.6");
-        $(".jctjitem li").eq(i).siblings().css("opacity","0.3");
-        
-      }
-      else {
-        i=3;
-        $(".bannerlist").css("left","-3950px");
-        $(".jctjitem li").eq(3).css("opacity","0.6");
-        $(".jctjitem li").eq(3).siblings().css("opacity","0.3");
-        
-      }
-    });
+    else {
+      i = 3;
+      $(".bannerlist").css("left", "-3950px");
+      $(".jctjitem li").eq(3).css("opacity", "0.6");
+      $(".jctjitem li").eq(3).siblings().css("opacity", "0.3");
+
+    }
+    swiper()
+  });
 });
